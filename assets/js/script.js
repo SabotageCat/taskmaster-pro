75eeab0ac29639387e011ead18ead3921b02c472
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-03-02 21:13:03 -0600

    added drag-drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -232,6 +232,22 @@ $(".card .list-group").sortable({
   }
 });
 
+//trash dropppable
+$("#trash").droppable({
+  accept:".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // load tasks for the first time
 loadTasks();
 
